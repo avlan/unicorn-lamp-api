a3947b0bb7c6914ee2b1877d5c5b916d75ea7d76
Carlos Avilan <avilancj@gmail.com>
2024-03-26 21:46:55 +0100

    Comment toggle for now, add change-color endpoint

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from lamps.lamp import Lamp
 app = FastAPI()
 lamp = Lamp()
 
-
 @app.get("/")
 def read_root():
     return {"version": "0.0.1"}
-
 
 @app.get("/switch", status_code=200)
 def turn_switch(v: str, response: Response):
@@ -37,16 +35,27 @@ def turn_off(response: Response):
 
     return {"status": "off"}
 
-@app.get("/toggle", status_code=200)
-def toggle(response: Response):
-    status = None
-    print(lamp.get_pixel(0,0))
-    if lamp.get_pixel(0,0) == 0 or lamp.get_pixel(0,0) == None:
-        lamp.set_all(255, 255, 255)
-        lamp.show()
-        status = "on"
-    else:
-        lamp.off()
-        status: "off"
 
-    return {"status": status}
+@app.get("/change-color", status_code=200)
+def change_color(r: str, g:str, b:str, response: Response):
+    red = int(r)
+    green = int(g)
+    blue = int(b)
+    lamp.set_all(red,green,blue)
+    lamp.show()
+
+    return {"status": "on"}
+
+# @app.get("/toggle", status_code=200)
+# def toggle(response: Response):
+#     status = None
+#     print(lamp.get_pixel(0,0))
+#     if lamp.get_pixel(0,0) == 0 or lamp.get_pixel(0,0) == None:
+#         lamp.set_all(255, 255, 255)
+#         lamp.show()
+#         status = "on"
+#     else:
+#         lamp.off()
+#         status: "off"
+
+#     return {"status": status}
